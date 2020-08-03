2e3487c6f50d6fa7e564bc1563ce5a45794850d9
cdshenlong1 <cdshenlong1@jd.com>
2020-08-03 11:36:45 +0800

    [learn]

--- springmvc-test/src/main/java/com/athena/sample/BootStrap.java
+++ springmvc-test/src/main/java/com/athena/sample/BootStrap.java
@@ -1,8 +1,10 @@
 package com.athena.sample;
 
+import com.athena.sample.model.DefaultEchoServiceImpl;
 import com.athena.sample.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.context.annotation.RequestScope;
 
@@ -13,7 +15,9 @@ import org.springframework.web.context.annotation.RequestScope;
 public class BootStrap {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootStrap.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(BootStrap.class, args);
+        DefaultEchoServiceImpl defaultEchoService = applicationContext.getBean("defaultEchoServiceImpl", DefaultEchoServiceImpl.class);
+        defaultEchoService.hello();
     }
 
     @Bean
